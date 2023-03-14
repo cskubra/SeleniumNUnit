06451f94d3258a0e9ecd34d71b9097ad15bcef25
Kubra <cskubra@gmail.com>
2023-03-15 01:52:48 +0300

    Add project files.

---
+++ SeleniumNUnit/MyFeature.feature
@@ -0,0 +1,13 @@
+﻿Feature: MyFeature
+
+A short summary of the feature
+
+@tag1
+Scenario: Amazon Test
+	Given User navigates to url "https://amazon.com"
+	When User input text "selenium test" on "//input[@id='twotabsearchtextbox']"
+	When User clicks on "//input[@type='submit']"
+	When User clicks on "//span[text()='Test Automation using Selenium WebDriver with Java: Step by Step Guide']"
+	Then User verify value of "//span[@xpath='1']" is "$28.60"
+	Then User closes the browser
+	
---
+++ SeleniumNUnit/StepDefinition.cs
@@ -0,0 +1,56 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SeleniumNUnit.StepDefinition
+{
+    [Binding]
+    internal class StepDefinition
+    {
+        IWebDriver webDriver;
+
+
+        [Given(@"User navigates to url ""([^""]*)""")]
+        public void GivenUserNavigatesToUrl(string url)
+        {
+            webDriver = new ChromeDriver(@"D:\WebDriver\Chrome"); 
+            webDriver.Manage().Window.Maximize();
+            webDriver.Navigate().GoToUrl(url);
+        }
+
+        [When(@"User input text ""([^""]*)"" on ""([^""]*)""")]
+        public void WhenUserInputTextOn(string text, string identifier)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(identifier));
+            webElement.SendKeys(text);
+        }
+
+        [When(@"User clicks on ""([^""]*)""")]
+        public void WhenUserClicksOn(string identifier)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(identifier));
+            webElement.Click();
+        }
+
+        [Then(@"User verify value of ""([^""]*)"" is ""([^""]*)""")]
+        public void ThenUserVerifyValueOf(string identifier, string expectedvalue)
+        {
+            IWebElement webElement = webDriver.FindElement(By.XPath(identifier));
+            Assert.That(webElement.Text, Is.EqualTo(expectedvalue));
+        }
+
+
+        [Then(@"User closes the browser")]
+        public void ThenUserClosesTheBrowser()
+        {
+            webDriver.Close();
+        }
+
+    }
+}
